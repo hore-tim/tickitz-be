35e13ace891ef2726f4517f6d0dc07b793a186de
redhadefinto <redhadefinto28@gmail.com>
2023-04-29 22:23:16 +0700

    create login, register, Forgot, reset-password, Logout

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const masterRouter = express.Router();
 
 const welcomeRouter = require("./welcome.route");
+const authRouter = require("./auth.route");
 
 masterRouter.use("/", welcomeRouter);
+masterRouter.use("/auth", authRouter);
 
 module.exports = masterRouter;
