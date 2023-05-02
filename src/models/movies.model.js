a21a92a1c0f0eddaf92f4b2c088c3aeebb107938
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-05-02 07:19:50 +0700

    fix movies

--- src/models/movies.model.js
+++ src/models/movies.model.js
@@ -2,9 +2,9 @@ const supabase = require('../configs/supabase')
 
 const addMovies = (data) => {
     return new Promise((resolve, reject) => {
-        const sql = `insert into movies (title, image, category, release_date, duration, director, casts, synopsis, user_id, created_at)
+        const sql = `insert into movies (title, image, category, release_date, duration, director, casts, synopsis, seller_id, created_at)
                     values ($1, $2, $3, $4, $5, $6, $7, $8, $9, now()) returning *`;
-        const values = [data.title, data.image, data.category, data.releaseDate, data.duration, data.director, data.casts, data.synopsis, data.userId ];
+        const values = [data.title, data.image, data.category, data.releaseDate, data.duration, data.director, data.casts, data.synopsis, data.sellerId ];
         supabase.query(sql, values, (err, result) => {
             if (err) {
                 return reject(err)
@@ -16,34 +16,35 @@ const addMovies = (data) => {
 
 const getAllMovies = (query) => {
     return new Promise((resolve, reject) => {
-        let sql = `select * from movies `;
+        let sql = `select m.title, m.image as movies_image, m.synopsis, m.duration, m.release_date, m.duration, m.director, m.casts, m.synopsis, m.seller_id, t.show_date, t.show_time, c."name" as cinema_name, c.image as cinema_image 
+        from movies m join cinemas c on c.movies_id = m.id join "time" t on t.cinemas_id = c.id `;
         if(query.show && query.show === 'now') {
-            sql += `where release_date <= now() `
+            sql += `where t.show_date <= now() `
         }
         if(query.show && query.show !== 'now') {
             let showDate = query.show.split('-')
-            sql += `where date_part('month', release_date)=${showDate[1]} and date_part('year', release_date)=${showDate[0]} `
+            sql += `where date_part('month', t.show_date)=${showDate[1]} and date_part('year', t.show_date)=${showDate[0]} `
         }
         switch (query.sort) {
             case "name_asc":
-                sql += `order by name asc `;
+                sql += `order by m.title asc `;
                 break;
             case "name_desc":
-                sql += `order by name desc `;
+                sql += `order by m.title desc `;
                 break;
             case "release_asc":
-                sql += `order by release_date asc `;
+                sql += `order by m.release_date asc `;
                 break;
             case "release_desc":
-                sql += `order by release_date desc `;
+                sql += `order by m.release_date desc `;
                 break;
             case "duration_asc":
-                sql += `order by duration asc `;
+                sql += `order by m.duration asc `;
                 break;
             case "duration_desc":
-                sql += `order by duration desc `;
+                sql += `order by m.duration desc `;
                 break;
-            default: sql += `order by name asc `;
+            default: sql += `order by m.title asc `;
         }
 
         const limit = Number(query.limit || 10);
@@ -63,7 +64,14 @@ const getAllMovies = (query) => {
 
 const getMetaMovies = (query) => {
     return new Promise((resolve, reject) => {
-        const sql = `select count(*) as total_movies from movies`;
+        let sql = `select count(*) as total_movies from movies m join cinemas c on c.movies_id = m.id join "time" t on t.cinemas_id = c.id `;
+        if(query.show && query.show === 'now') {
+            sql += `where t.show_date <= now() `
+        }
+        if(query.show && query.show !== 'now') {
+            let showDate = query.show.split('-')
+            sql += `where date_part('month', t.show_date)=${showDate[1]} and date_part('year', t.show_date)=${showDate[0]} `
+        }
         let endpoint = `/movies?`;
         if (query.sort !== undefined) {
             endpoint += `sort=${query.sort}&`;
@@ -79,26 +87,7 @@ const getMetaMovies = (query) => {
             const page = Number(query.page || 1);
             const dataLimit = Number(query.limit || 10);
             const totalPage = Math.ceil(totalMovies / dataLimit);
-            // switch (query.sort) {
-            //     case "name_asc":
-            //         endpoint += `sort=name_asc&`;
-            //         break;
-            //     case "name_desc":
-            //         endpoint += `sort=name_desc&`;
-            //         break;
-            //     case "release_asc":
-            //         endpoint += `sort=release_asc&`;
-            //         break;
-            //     case "release_desc":
-            //         endpoint += `sort=release_desc&`;
-            //         break;
-            //     case "duration_asc":
-            //         endpoint += `sort=duration_asc&`;
-            //         break;
-            //     case "duration_desc":
-            //         endpoint += `sort=duration_desc&`;
-            //         break;
-            // }
+            
             let prev = `${endpoint}limit=${dataLimit}&page=${page - 1}`;
             let next = `${endpoint}limit=${dataLimit}&page=${page + 1}`;
             if (page === 1) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,11 @@ const express = require("express");
 const masterRouter = express.Router();
 
 const welcomeRouter = require("./welcome.route");
-<<<<<<< HEAD
 const moviesRouter = require("./movies.route")
-=======
 const authRouter = require("./auth.route");
 const profileRouter = require("./profile.route");
 const bookingRouter = require("./booking.route");
 const seatRouter = require("./seat.route");
->>>>>>> edac1bf7a1b276e33fe0e080ffb957761b79204a
 
 masterRouter.use("/movies", moviesRouter);
 masterRouter.use("/", welcomeRouter);
