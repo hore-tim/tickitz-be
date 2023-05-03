4580a24a18eef50bbfcc9c2ea40be2a6a0f3864a
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-05-03 16:08:28 +0700

    add cinemas route

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,8 @@ const authRouter = require("./auth.route");
 const profileRouter = require("./profile.route");
 const bookingRouter = require("./booking.route");
 const seatRouter = require("./seat.route");
-const showRouter = require('./show.routes')
+const showRouter = require('./show.routes');
+const cinemasRouter = require('./cinemas.route')
 
 masterRouter.use("/movies", moviesRouter);
 masterRouter.use("/", welcomeRouter);
@@ -16,5 +17,6 @@ masterRouter.use("/auth", authRouter);
 masterRouter.use("/profile", profileRouter);
 masterRouter.use("/booking", bookingRouter);
 masterRouter.use("/seat", seatRouter);
+masterRouter.use("/cinemas", cinemasRouter);
 
 module.exports = masterRouter;
