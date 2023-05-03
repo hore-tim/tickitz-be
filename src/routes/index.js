fbada423eaf3e662db2674ee7625b29e82790ec4
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-05-03 13:49:14 +0700

    add show

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,11 @@ const authRouter = require("./auth.route");
 const profileRouter = require("./profile.route");
 const bookingRouter = require("./booking.route");
 const seatRouter = require("./seat.route");
+const showRouter = require('./show.routes')
 
 masterRouter.use("/movies", moviesRouter);
 masterRouter.use("/", welcomeRouter);
+masterRouter.use("/show", showRouter);
 masterRouter.use("/auth", authRouter);
 masterRouter.use("/profile", profileRouter);
 masterRouter.use("/booking", bookingRouter);
